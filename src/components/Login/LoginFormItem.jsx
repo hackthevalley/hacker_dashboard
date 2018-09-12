3f3895d0e685cf8d59c79e1892dc685a075f5df7
Omar Chehab <omarchehab98@gmail.com>
2018-09-11 23:20:41 -0400

    Update LoginFormItem component
    Allow input element's props to be changed through the inputProps prop.

--- src/components/Login/LoginFormItem.jsx
+++ src/components/Login/LoginFormItem.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 export class LoginFormItem extends React.Component {
+  static defaultProps = {
+    inputProps: {},
+  }
+
   render() {
-    const { id, label, type, block } = this.props;
+    const {
+      id,
+      label,
+      type,
+      block,
+      inputProps,
+    } = this.props;
     return <div className={`${block}__item`}>
       <label className={`${block}__label`} htmlfor={`#${id}`}>{ label }</label>
-      <input id={id} type={type? type: "text"} className={`${block}__input`}/>
+      <input
+        id={id}
+        type={type ? type : "text"}
+        className={`${block}__input`}
+        {...inputProps}
+      />
     </div>
   }
 }
