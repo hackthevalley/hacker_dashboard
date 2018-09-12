50e5a87936e88af4d6e2b11dde809b270543dc73
Omar Chehab <omarchehab98@gmail.com>
2018-09-11 23:27:12 -0400

    Update App container
    If the user is not logged in and is on the dashboard page, then redirect user to login page.
    If the user is logged in and is on the login page, then redirect user to the dashboard page.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,17 +1,36 @@
 import React, { Component} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Dashboard, Login } from './../pages';
+import { IsLoggedIn, IsNotLoggedIn } from '../containers';
 import '../css/containers/app.css';
+
+function requireLoggedIn(SomeComponent) {
+  return () => (
+    <IsLoggedIn>
+      <SomeComponent />
+    </IsLoggedIn>
+  )
+}
+
+function requireNotLoggedIn(SomeComponent) {
+  return () => (
+    <IsNotLoggedIn>
+      <SomeComponent />
+    </IsNotLoggedIn>
+  )
+}
 
 export class App extends Component {
   render() {
-    return <Switch>
-      <Route exact path="/" component={Login}/>
-      <Route exact path="/home" component={Dashboard}/>
-      <Route exact path="/profile" component={Dashboard}/>
-      <Route exact path="/app" component={Dashboard}/>
-      <Route exact path="/faq" component={Dashboard}/>
-      <Route exact path="/contact" component={Dashboard}/>
-    </Switch>;
+    return (
+      <Switch>
+        <Route exact path="/" component={requireNotLoggedIn(Login)}/>
+        <Route exact path="/home" component={requireLoggedIn(Dashboard)}/>
+        <Route exact path="/profile" component={requireLoggedIn(Dashboard)}/>
+        <Route exact path="/app" component={requireLoggedIn(Dashboard)}/>
+        <Route exact path="/faq" component={requireLoggedIn(Dashboard)}/>
+        <Route exact path="/contact" component={requireLoggedIn(Dashboard)}/>
+      </Switch>
+    );
   }
 }
