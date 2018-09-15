d635bf6cf8e49c9d8fa3d2c4d4e0717f7f11f20e
Omar Chehab <omarchehab98@gmail.com>
2018-09-15 13:27:08 -0400

    Update createHackerTokenAction, use htv-sdk

--- src/redux/actions/createHackerTokenAction.js
+++ src/redux/actions/createHackerTokenAction.js
@@ -1,35 +1,23 @@
-import { graphqlQueryAction } from './';
+import htv from 'htv-sdk';
+import HttpRequestError from '../../errors/HttpRequestError';
 
 export const CREATEHACKERTOKEN_FAIL = 'CREATEHACKERTOKEN_FAIL';
 export const CREATEHACKERTOKEN_SUCCESS = 'CREATEHACKERTOKEN_SUCCESS';
 
 export function createHackerTokenAction(email_address, password) {
   return async (dispatch) => {
-    const action = await dispatch(graphqlQueryAction({
-      query: `mutation createHackerTokenMutation($email_address: String!, $password: String!) {
-        createHackerToken(email_address: $email_address, password: $password) {
-          token_body
-        }
-      }`,
-
-      variables: {
+    try {
+      const token_body = await htv.Hacker.createToken(email_address, password);
+      return dispatch({
+        type: CREATEHACKERTOKEN_SUCCESS,
         email_address,
-        password,
-      },
-    }));
-
-    if (action.error) {
+        token_body,
+      });
+    } catch (error) {
       return dispatch({
         type: CREATEHACKERTOKEN_FAIL,
-        error: action.error,
+        error: new HttpRequestError(error.graphQLErrors.map(err => err.message)),
       });
     }
-
-    const actionResponse = action.data.createHackerToken;
-    return dispatch({
-      type: CREATEHACKERTOKEN_SUCCESS,
-      email_address,
-      token_body: actionResponse.token_body,
-    })
   }
 }
