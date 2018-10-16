1757fe577fb5028ac6afd22a8995765866edd859
Frederic Pun <frederic.pun@gmail.com>
2018-10-16 14:08:38 -0400

    Updates

---
+++ precache-manifest.631d9f045ff2588008c2fcd8f1f690e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4193677482d7d723cca31feb11eda423",
+    "url": "/hack_the_valley_dashboard/static/media/profile.41936774.jpg"
+  },
+  {
+    "revision": "f21b64452c5f764b1d5b",
+    "url": "/hack_the_valley_dashboard/static/js/runtime~main.f21b6445.js"
+  },
+  {
+    "revision": "46df75b85ffcbf265b92",
+    "url": "/hack_the_valley_dashboard/static/js/main.46df75b8.chunk.js"
+  },
+  {
+    "revision": "60f8edabaa66ca2b5484",
+    "url": "/hack_the_valley_dashboard/static/js/1.60f8edab.chunk.js"
+  },
+  {
+    "revision": "46df75b85ffcbf265b92",
+    "url": "/hack_the_valley_dashboard/static/css/main.8d8508fa.chunk.css"
+  },
+  {
+    "revision": "414e4075582e19ae0b60c1d0158e4347",
+    "url": "/hack_the_valley_dashboard/index.html"
+  }
+];
